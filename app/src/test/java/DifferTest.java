19891232a98e04fef3e7c5f190b8bf8dd4b0e367
v-b-a <valiakhmedov.ba@gmail.com>
2022-10-05 18:41:26 +0300

    correct test

--- app/src/test/java/DifferTest.java
+++ app/src/test/java/DifferTest.java
@@ -10,9 +10,9 @@ public class DifferTest {
     private final String absoluteFileJSONPath2 =
             "src/test/resources/file2.json";
     private final String absoluteFileYAMLPath1 =
-            "src/test/resources/yamlFile1.yaml";
+            "src/test/resources/fixtures/yamlFile1.yaml";
     private final String absoluteFileYAMLPath2 =
-            "src/test/resources/yamlFile2.yaml";
+            "src/test/resources/fixtures/yamlFile1.yaml";
     private static final String DIFF_STYLISH = """
             {
                 chars1: [a, b, c]
@@ -41,17 +41,29 @@ public class DifferTest {
             }""";
 
     private static final String DIFF_STYLISH_2 = "{\n" +
-            "    aboolean: true\n" +
-            "  - foo: bar\n" +
-            "  + foo: bar2\n" +
-            "  - fry: [1, 2, 3, 4]\n" +
-            "  + fry: [1, 2, 3, 4, 5]\n" +
-            "  - int: 2\n" +
-            "    new prop1: new prop value\n" +
-            "  + numbers2: [2, 3, 4, 5]\n" +
-            "    one: In Treatment\n" +
-            "    rar: null\n" +
-            "  + two: The Wire\n" +
+            "    chars1: [a, b, c]\n" +
+            "  - chars2: [d, e, f]\n" +
+            "  + chars2: false\n" +
+            "  - checked: false\n" +
+            "  + checked: true\n" +
+            "  - default: null\n" +
+            "  + default: [value1, value2]\n" +
+            "  - id: 45\n" +
+            "  + id: null\n" +
+            "  - key1: value1\n" +
+            "  + key2: value2\n" +
+            "    numbers1: [1, 2, 3, 4]\n" +
+            "  - numbers2: [2, 3, 4, 5]\n" +
+            "  + numbers2: [22, 33, 44, 55]\n" +
+            "  - numbers3: [3, 4, 5]\n" +
+            "  + numbers4: [4, 5, 6]\n" +
+            "  + obj1: {nestedKey=value, isNested=true}\n" +
+            "  - setting1: Some value\n" +
+            "  + setting1: Another value\n" +
+            "  - setting2: 200\n" +
+            "  + setting2: 300\n" +
+            "  - setting3: true\n" +
+            "  + setting3: none\n" +
             "}";
     private static final String DIFF_PLAIN = """
             Property 'chars2' was updated. From [complex value] to false
