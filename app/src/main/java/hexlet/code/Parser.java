5ae8b4c8a5a1da1ac3f76fcb9215dac368eca8ae
v-b-a <valiakhmedov.ba@gmail.com>
2022-10-18 20:13:02 +0300

    fix checkstyle

--- app/src/main/java/hexlet/code/Parser.java
+++ app/src/main/java/hexlet/code/Parser.java
@@ -13,31 +13,20 @@ public class Parser {
     public static Map<String, Object> parsing(Path filepath, String fileExtension) throws Exception {
         String stringFile = Files.readString(filepath);
         Map<String, Object> result;
-        if (fileExtension.equals(FilesExtension.JSON.getExtension())) {
-            ObjectMapper objectMapper = new ObjectMapper();
-            result = objectMapper.readValue(stringFile, new TypeReference<>() {
-            });
-        } else if (fileExtension.equals(FilesExtension.YAML.getExtension()) ||
-                fileExtension.equals(FilesExtension.YML.getExtension())) {
-            ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-            result = objectMapper.readValue(stringFile, new TypeReference<>() {
-            });
-        } else {
-            throw new Exception("Ошибка формата");
+        enum extensions {json, yaml, yml}
+        extensions cFileExtension = extensions.valueOf(fileExtension);
+        switch (cFileExtension) {
+            case json:
+                ObjectMapper objectMapper = new ObjectMapper();
+                result = objectMapper.readValue(stringFile, new TypeReference<>() {});
+                break;
+            case yml, yaml:
+                objectMapper = new ObjectMapper(new YAMLFactory());
+                result = objectMapper.readValue(stringFile, new TypeReference<>() {});
+                break;
+            default:
+                throw new Exception("Расширения не поддерживаются");
         }
-//        FilesExtension filesExtension2 = FilesExtension.valueOf(fileExtension);  // другого способа работать в свиче с енам не нашел
-//        switch (filesExtension2) {
-//            case FilesExtension.JSON.getExtension():
-//                objectMapper = new ObjectMapper();
-//                result = objectMapper.readValue(stringFile, new TypeReference<>() {});
-//                break;
-//            case FilesExtension.YAML.getExtension(), FilesExtension.YML.getExtension():
-//                objectMapper = new ObjectMapper(new YAMLFactory());
-//                result = objectMapper.readValue(stringFile, new TypeReference<>() {});
-//                break;
-//            default:
-//                throw new Exception("throw new Exception("Гавно");");
-//        }
         return result;
     }
 
