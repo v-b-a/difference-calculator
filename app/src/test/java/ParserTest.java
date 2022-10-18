f5229c3c4664cec26cd2ba76621cd709ad817b78
v-b-a <valiakhmedov.ba@gmail.com>
2022-10-18 20:19:38 +0300

    fix checkstyle4

--- app/src/test/java/ParserTest.java
+++ app/src/test/java/ParserTest.java
@@ -3,7 +3,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
